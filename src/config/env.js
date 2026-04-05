93c141db8d727248e6e3f89d8d5d39ac1be3d898
Jeeshan Khan <jeeshankhan3232@gmail.com>
2026-04-06 00:10:09 +0530

    fix base url

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_BASE_URL = "http://localhost:2000";
+const DEFAULT_API_BASE_URL = "https://zorvyn-backend-coral.vercel.app";
 
 export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || DEFAULT_API_BASE_URL;
